cbc7d26b3d1c775105aa97b7a1c4c6cb428af748
yeahg-dev <yeahg.dev@gmail.com>
2022-03-12 16:11:12 +0900

    [inset/offset] CGRect의 inset, offset 메서드 역할 알아보기 #1

---
+++ UIKit-basic/UIKit-basic/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  UIKit-basic
+//
+//  Created by 1 on 2022/03/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var rectangularView: UIView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.adjustViewByInset()
+//        self.adjustViewByOffset()
+    }
+    
+    func adjustViewByInset() {
+        print(self.rectangularView.frame)
+        let inests = UIEdgeInsets(top: 30, left: 30, bottom: 30, right: 30)
+        self.rectangularView.frame = rectangularView.frame.inset(by: inests)
+        print(rectangularView.frame) // (87.0, 403.0, 240.0, 90.0)
+    }
+    
+    func adjustViewByOffset() {
+        print(self.rectangularView.frame)
+        self.rectangularView.frame = rectangularView.frame.offsetBy(dx: 30, dy: 30)
+        print(self.rectangularView.frame)
+    }
+
+}
+
